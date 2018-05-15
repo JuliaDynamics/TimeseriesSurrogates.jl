ba1800dec042378007e1bfacda43eddf7de61c7e
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-05-15 16:22:10 +0200

    Interpolation for periodograms

---
+++ src/interpolation.jl
@@ -0,0 +1,14 @@
+"""
+Linearly interpolates two vector x and y on a linear grid with binsizes given by `by`.
+"""
+function intp(x::Vector, y::Vector, nsteps::Int)
+    # Interpolate
+    itp = interpolate((x,), y, Gridded(Linear()))
+    # Interpolate at the given resolution
+    x_fills = linspace(minimum(x), maximum(x), nsteps)
+    y_fills = itp[x_fills]
+
+    return collect(x_fills), y_fills
+end
+
+export intp
