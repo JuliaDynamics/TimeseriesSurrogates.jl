15f21551c5ee870b788bf062d134ea88e6b4fb9d
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-05-15 16:24:49 +0200

    Fix tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,18 +2,21 @@ using Base.Test
 using TimeseriesSurrogates
 
 ts = cumsum(randn(1000))
+
 @testset "Constrained surrogates" begin
     @testset "Random shuffle" begin
         surrogate = randomshuffle(ts)
         @test length(ts) == length(surrogate)
-        @test all(ts .!= surrogate)
         @test all(sort(ts) .== sort(surrogate))
+        @test !all(ts .== surrogate)
     end
 
     @testset "Random amplitudes" begin
         surrogate = randomphases(ts)
         @test length(ts) == length(surrogate)
         @test all(ts .!= surrogate)
+    end
+
         @test all(sort(ts) .== sort(surrogate))
     end
 end
