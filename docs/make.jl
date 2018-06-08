e2a17c6935c4c5dc9b458349bbde003559b0e07a
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-06-08 12:38:27 +0200

    Try to get docs pushed to github pages

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,11 @@
 using Documenter, TimeseriesSurrogates
-#
-# makedocs(modules = [TimeseriesSurrogates],
-#          doctest = true)
-#
-# deploydocs(deps = Deps.pip("mkdocs", "python-markdown-math"),
-#            repo = "github.com/kahaaga/TimeseriesSurrogates.git",
-#            julia  = "0.6",
-#            osname = "linux")
 ENV["GKSwstype"] = "100"
 
 makedocs(format = :html,
         sitename = "TimeseriesSurrogates docs")
 
 deploydocs(
-    #deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    deps   = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
     repo   = "github.com/kahaaga/TimeseriesSurrogates.jl.git",
     julia  = "0.6",
     osname = "linux"
