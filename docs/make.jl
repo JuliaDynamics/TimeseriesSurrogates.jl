2d8f2d674c61893b44db01abbc8b223f67cc785c
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-06-08 15:11:13 +0200

    Specify page.

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,10 @@ using Documenter, TimeseriesSurrogates
 ENV["GKSwstype"] = "100"
 
 makedocs(format = :html,
-        sitename = "TimeseriesSurrogates docs")
+        sitename = "TimeseriesSurrogates docs",
+        pages = [
+            "Index" => "index.md"
+        ])
 
 deploydocs(
     deps   = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
