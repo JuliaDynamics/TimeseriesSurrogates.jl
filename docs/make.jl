64641c9be40c83195ac41da0d25f2afff3fe5351
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-07-23 11:18:45 +0200

    Try generating docs without mkdocs.yml

--- docs/make.jl
+++ docs/make.jl
@@ -1,33 +1,33 @@
 using Documenter, TimeseriesSurrogates
-#push!(LOAD_PATH,"../src/")
 ENV["GKSwstype"] = "100"
+
+
+PAGES = [
+    "Overview" => "index.md",
+    "What is a surrogate?" => "man/whatisasurrogate.md",
+    "Surrogate algorithms" => [
+        "Random shuffle (RS)" => "constrained/randomshuffle.md",
+        "Fourier transform (FT)" => "constrained/fourier_surrogates.md",
+        "Amplitude adjusted Fourier (AAFT)" => "constrained/amplitude_adjusted.md"
+    ],
+    "Visualising surrogate realizations and creating gifs" => "plots/visualize_surrogate.md"
+]
 
 makedocs(
     modules = [TimeseriesSurrogates],
     format = :html,
     sitename = "TimeseriesSurrogates.jl",
     authors = "Kristian Agasøster Haaga",
-    pages = [
-        "Overview" => "index.md",
-        "What is a surrogate?" => "man/whatisasurrogate.md",
-        "Surrogate algorithms" => [
-            "Random shuffle (RS)" => "constrained/randomshuffle.md",
-            "Fourier transform (FT)" => "constrained/fourier_surrogates.md",
-            "Amplitude adjusted Fourier (AAFT)" => "constrained/amplitude_adjusted.md"
-        ],
-        "Visualising surrogate realizations and creating gifs" => "plots/visualize_surrogate.md"
-    ],
+    pages = PAGES
     # Use clean URLs, unless built as a "local" build
-    html_prettyurls = !("local" in ARGS),
-    html_canonical = "https://kahaaga.github.io/TimeseriesSurrogates.jl/latest/"
+    #html_prettyurls = !("local" in ARGS),
+    #html_canonical = "https://kahaaga.github.io/TimeseriesSurrogates.jl/latest/"
 )
 
 deploydocs(
     repo   = "github.com/kahaaga/TimeseriesSurrogates.jl.git",
     julia  = "0.6",
     osname = "linux",
-    branch = "gh-pages",
-    latest = "master",
-    #deps = nothing,
-    #make = nothing,
+    deps = nothing,
+    make = nothing,
 )
