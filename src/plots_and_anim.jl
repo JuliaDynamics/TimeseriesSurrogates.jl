10b31382fe9626bd8bdc540f34c2d01550bccb0c
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-11-14 21:52:21 +0100

    Fix docs.

--- src/plots_and_anim.jl
+++ src/plots_and_anim.jl
@@ -7,11 +7,11 @@ for surrogate_type in surrogate_methods
     ############################
     fname1 = Symbol("$(surrtype)_plot")
     @eval begin
-        @doc """
+        """
                 `$($surrtype)_plot(ts)`
 
             Visualize a `$($surrtype)` surrogate.
-        """ ->
+        """
         function $(fname1)(ts)
             surrplot(ts, $(surrtype)(ts))
         end
@@ -19,11 +19,11 @@ for surrogate_type in surrogate_methods
 
     fname2 = Symbol("$(surrtype)_anim")
     @eval begin
-        @doc """
+        """
                 `$($surrtype)_anim(ts; [n_iters::Int])`
 
             Create `n_iters` `$($surrtype)` surrogates for `ts` and animate them. Returns a `Plots.Animation` instance.
-        """ ->
+        """
         function $(fname2)(ts; n_iters = 15)
             anim = Plots.@animate for i = 1:n_iters
                 surrplot(ts, $(surrtype)(ts))
@@ -34,12 +34,12 @@ for surrogate_type in surrogate_methods
 
     fname2b = Symbol("$(surrtype)_gif")
     @eval begin
-        @doc """
+        """
                 `$($surrtype)_gif(ts; [n_iters::Int])`
 
             Create `n_iters` `$($surrtype)` surrogates for `ts`, animate them and create
             a gif from the animation.
-        """ ->
+        """
         function $(fname2b)(ts; fps = 2, n_iters = 15)
             anim = Plots.@animate for i = 1:n_iters
                 surrplot(ts, $(surrtype)(ts))
@@ -63,11 +63,11 @@ for surrogate_type in surrogate_methods
         # Surrogate panel plots for combinations of processes and surrogate types
         fname3 = Symbol("$(surrtype)_$(process)_plot")
         @eval begin
-            @doc """
+            """
                     `$($fname3)(; [fps, n_iters, n_steps, new_realization_every_iter::Bool])`
 
                 Create a `$($surrtype)` surrogate for a realization of a `$($proc)` process and visualize it.
-            """ ->
+            """
             function $(fname3)(;n_iters = 15,
                                 n_steps = 500,
                                 new_realization_every_iter::Bool = false)
@@ -81,13 +81,13 @@ for surrogate_type in surrogate_methods
         # Animate surrogate generation for different surrogate types and processes
         fname4 = Symbol("$(surrtype)_$(process)_anim")
         @eval begin
-            @doc """
+            """
                     `$($fname4)(; [fps, n_iters, n_steps, new_realization_every_iter::Bool])`
 
                 Create `n_iters` `$($surrtype)` surrogates for realizations of a `$($proc)` process and animate them. Returns a `Plots.Animation` instance.
 
                 If `new_realization_every_iter = true`, then a fresh `$($proc)` realization is used at each iteration. Otherwise, the same time series, generating only the surrogates anew at each iteration.
-            """ ->
+            """
             function $(fname4)(;n_iters = 15,
                                 n_steps = 500,
                                 new_realization_every_iter::Bool = false)
@@ -109,14 +109,14 @@ for surrogate_type in surrogate_methods
         # Animate surrogate generation for different surrogate types and processes
         fname4b = Symbol("$(surrtype)_$(process)_gif")
         @eval begin
-            @doc """
+            """
                     `$($fname4b)(; [fps, n_iters, n_steps, new_realization_every_iter::Bool])`
 
                 Create `n_iters` `$($surrtype)` surrogates for realizations of a `$($proc)`
                 process, animate them and create a gif from the animation.
 
                 If `new_realization_every_iter = true`, then a fresh `$($proc)` realization is used at each iteration. Otherwise, the same time series, generating only the surrogates anew at each iteration.
-            """ ->
+            """
             function $(fname4b)(;fps = 2, n_iters = 15,
                                 n_steps = 500,
                                 new_realization_every_iter::Bool = false)
