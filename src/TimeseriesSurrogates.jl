9b4ea97260823e342d860caa1ad4f594f54486e9
George Datseris <datseris.george@gmail.com>
2019-12-19 13:11:12 +0100

    put Plots behind a requires

--- src/TimeseriesSurrogates.jl
+++ src/TimeseriesSurrogates.jl
@@ -8,7 +8,6 @@ using DSP
 using Interpolations
 using Wavelets
 using Requires
-using Plots
 
 # Example systems
 include("testsystems.jl")
@@ -16,8 +15,7 @@ include("testsystems.jl")
 # Periodogram interpolation
 include("interpolation.jl")
 
-# Visualization routine for time series + surrogate + periodogram/acf/histogram
-include("surrogate_plot.jl")
+include("uncertaindatasets.jl")
 
 # The different surrogate routines
 include("randomshuffle.jl")
@@ -27,16 +25,20 @@ include("aaft.jl")
 include("iaaft.jl")
 include("wiaaft.jl")
 
-
-# Define and export plot routines for all combinations of example processes and surrogate
-# types
-processes = (:AR1, :NSAR2, :NLNS, :randomwalk, :SNLST)
-surrogate_methods = (:randomshuffle, :randomphases, :randomamplitudes, :aaft, :iaaft)
-include("plots_and_anim.jl")
-
-include("uncertaindatasets.jl")
-
 export NLNS, NSAR2, AR1, randomwalk, SNLST,
         randomshuffle, randomamplitudes, randomphases, aaft, iaaft, wiaaft
+
+# Visualization routine for time series + surrogate + periodogram/acf/histogram
+using Requires
+function __init__()
+    @require Plots="91a5bcdd-55d7-5caf-9e0b-520d859cae80" begin
+        # Define and export plot routines for all combinations of example processes and surrogate
+        # types
+        processes = (:AR1, :NSAR2, :NLNS, :randomwalk, :SNLST)
+        surrogate_methods = (:randomshuffle, :randomphases, :randomamplitudes, :aaft, :iaaft)
+        include("surrogate_plot.jl")
+        include("plots_and_anim.jl")
+    end
+end
 
 end # module
