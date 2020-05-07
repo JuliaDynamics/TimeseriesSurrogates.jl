5d36e8cde771bc2e434113d50b9cee15a0a5d5f7
Kristian Agasøster Haaga <kahaaga@gmail.com>
2020-05-07 23:01:14 +0200

    test for new api

--- test/runtests.jl
+++ test/runtests.jl
@@ -51,3 +51,69 @@ ts_nan[1] = NaN
         wiaaft(ts)
     end
 end
+
+@testset "New API" begin
+    @testset "Random shuffle" begin
+        method = RandomShuffle(ts)
+        surr = surrogate(ts, method)
+        @test length(ts) == length(surr)
+        @test all(sort(ts) .== sort(surr))
+        @test !all(ts .== surr)
+
+    end
+
+    @testset "Random phases" begin
+        # With pre-planning
+        method = RandomFourier(ts, true)
+        surr = surrogate(ts, method)
+        @test length(ts) == length(surr)
+        @test !all(ts .== surr)
+
+        # Without pre-planning
+        method = RandomFourier(true)
+        surr = surrogate(ts, method)
+        @test length(ts) == length(surr)
+        @test !all(ts .== surr)
+    end
+
+    @testset "Random amplitudes" begin
+       # With pre-planning
+       method = RandomFourier(ts, false)
+       surr = surrogate(ts, method)
+       @test length(ts) == length(surr)
+       @test !all(ts .== surr)
+
+       # Without pre-planning
+       method = RandomFourier(false)
+       surr = surrogate(ts, method)
+       @test length(ts) == length(surr)
+       @test !all(ts .== surr)
+    end
+
+    @testset "AAFT" begin
+        # With pre-planning
+        method = AAFT(ts, true)
+        surr = surrogate(ts, method)
+        @test length(ts) == length(surr)
+        @test all(sort(ts) .== sort(surr))
+	    @test_throws DomainError aaft(ts_nan)
+    end
+
+    @testset "IAAFT" begin
+        # With pre-planning
+        method = IAAFT(ts)
+        surr = surrogate(ts, method)
+        @test length(ts) == length(surr)
+        @test all(sort(ts) .== sort(surr))
+
+         # Without pre-planning
+         method = IAAFT()
+         surr = surrogate(ts, method)
+         @test length(ts) == length(surr)
+         @test all(sort(ts) .== sort(surr))
+    end
+
+    @testset "WIAAFT" begin
+        wiaaft(ts)
+    end
+end
