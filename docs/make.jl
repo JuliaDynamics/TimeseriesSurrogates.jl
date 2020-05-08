0e1fc5db2e16da95e9b49ce592877007f038f6ac
Datseris <datseris.george@gmail.com>
2020-05-08 23:16:35 +0200

    update doc theme to JuliaDynamics, use Project

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,33 @@
-using Documenter, TimeseriesSurrogates
-ENV["GKSwstype"] = "100"
-push!(LOAD_PATH,"../src/")
+cd(@__DIR__)
+using Pkg
+CI = get(ENV, "CI", nothing) == "true" || get(ENV, "GITHUB_TOKEN", nothing) !== nothing
+CI && Pkg.activate(@__DIR__)
+CI && Pkg.instantiate()
+CI && (ENV["GKSwstype"] = "100")
+using TimeseriesSurrogates
+using Plots
+using Documenter
+using DocumenterTools: Themes
+
+# %% JuliaDynamics theme.
+# download the themes
+using DocumenterTools: Themes
+for file in ("juliadynamics-lightdefs.scss", "juliadynamics-darkdefs.scss", "juliadynamics-style.scss")
+    download("https://raw.githubusercontent.com/JuliaDynamics/doctheme/master/$file", joinpath(@__DIR__, file))
+end
+# create the themes
+for w in ("light", "dark")
+    header = read(joinpath(@__DIR__, "juliadynamics-style.scss"), String)
+    theme = read(joinpath(@__DIR__, "juliadynamics-$(w)defs.scss"), String)
+    write(joinpath(@__DIR__, "juliadynamics-$(w).scss"), header*"\n"*theme)
+end
+# compile the themes
+Themes.compile(joinpath(@__DIR__, "juliadynamics-light.scss"), joinpath(@__DIR__, "src/assets/themes/documenter-light.css"))
+Themes.compile(joinpath(@__DIR__, "juliadynamics-dark.scss"), joinpath(@__DIR__, "src/assets/themes/documenter-dark.css"))
+
+# %% Build docs
+cd(@__DIR__)
+ENV["JULIA_DEBUG"] = "Documenter"
 
 PAGES = [
     "Overview" => "index.md",
@@ -16,20 +43,21 @@ PAGES = [
 
 makedocs(
     modules = [TimeseriesSurrogates],
-    format = :html,
+    format = Documenter.HTML(
+        prettyurls = CI,
+        assets = [
+            asset("https://fonts.googleapis.com/css?family=Montserrat|Source+Code+Pro&display=swap", class=:css),
+        ],
+        ),
     sitename = "TimeseriesSurrogates.jl",
-    authors = "Kristian Agasøster Haaga",
+    authors = "Kristian Agasøster Haaga, George Datseris",
     pages = PAGES
-    # Use clean URLs, unless built as a "local" build
-    #html_prettyurls = !("local" in ARGS),
-    #html_canonical = "https://kahaaga.github.io/TimeseriesSurrogates.jl/latest/"
 )
 
-deploydocs(
-    repo   = "github.com/kahaaga/TimeseriesSurrogates.jl.git",
-    julia  = "0.6",
-    target = "build",
-    deps = nothing,
-    make = nothing,
-    osname = "linux"
-)
+if CI
+    deploydocs(
+        repo = "github.com/JuliaDynamics/Agents.jl.git",
+        target = "build",
+        push_preview = true
+    )
+end
