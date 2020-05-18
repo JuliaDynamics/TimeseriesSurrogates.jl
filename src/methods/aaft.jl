80f84990edd5226afc40783c68411e0a3fcb5bb4
Datseris <datseris.george@gmail.com>
2020-05-18 15:11:22 +0200

    add hypothesis for AAFT

--- src/methods/aaft.jl
+++ src/methods/aaft.jl
@@ -6,6 +6,10 @@ An amplitude-adjusted-fourier-transform surrogate[^Theiler1992].
 
 AAFT have the same linear correlation, or periodogram, and also
 preserves the amplitude distribution of the original data.
+
+AAFT can be used to test the null hypothesis that the data come from a
+monotonic nonlinear transformation of a linear Gaussian process
+(also called integrated white noise).
 """
 struct AAFT <: Surrogate end
 
