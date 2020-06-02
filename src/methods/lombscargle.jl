d28916b78ffb863fb93982f7ccb84d18499e7ec2
Felix Cremer <felix.cremer@uni-jena.de>
2020-06-02 11:59:20 +0200

    Implement SurroGenerator API for lombscargle surr
    This deletes the surrogate function for signal and time axis and puts
    the time axis into the LS type. We are also following the surrogenerator
    api with LS. The arguments to the LS method are now incorporated into
    the method struct.

--- src/methods/lombscargle.jl
+++ src/methods/lombscargle.jl
@@ -2,44 +2,72 @@ using LombScargle
 
 export LS
 """
-
-Compute a surrogate of the irregular time series x with supporting time steps t
+LS(t; tol=1, N_total=10000, N_acc=2000,q=1)
+Compute a surrogate of the irregular time series with supporting time steps t
 based on the simulated annealing method described in
 
-Testing for nonlinearity in unevenly sampled time seriesAndreas Schmitz and Thomas Schreiber
+[^SchmitzSchreiber1999] A.Schmitz T.Schreiber (1999). "Testing for nonlinearity in unevenly sampled time series" [Phys. Rev E](https://journals.aps.org/pre/pdf/10.1103/PhysRevE.59.4044)
 """
-struct LS <: Surrogate end
+struct LS{T<:AbstractVector,S<:Real} <: Surrogate
+    t::T
+    tol::S
+    N_total::Int
+    N_acc::Int
+    q::Int
+end
 
-function loss(ls1, signal2,taxis;q=1)
-    #lsplan1 = LombScargle.plan(taxis, signal1)
-    lsplan2 = LombScargle.plan(taxis, signal2)
-    #ls1 = lombscargle(lsplan1)
-    ls2 = lombscargle(lsplan2)
-    #@show ls1.power .- ls2.power
-    return minkowski(ls1.power, ls2.power,q)
+LS(t;tol=1.0, N_total=10000, N_acc=5000, q=1) = LS(t, tol, N_total, N_acc,q)
+
+
+function surrogenerator(x, method::LS)
+    lsplan = LombScargle.plan(method.t, x, fit_mean=false)
+    x_ls = lombscargle(lsplan)
+    xpower = x_ls.power
+    @show sum(xpower)
+    dist=Minkowski(method.q)
+    init = (lsplan=lsplan, xpower=xpower, n=length(x), dist=dist)
+    return SurrogateGenerator(method, x, init)
 end
 
 
-
-function surrogate(signal,taxis,sg::LS; N_total=10000, N_acc=2000, tol=1)
-    n = length(signal)
-    surr = surrogate(signal, RandomShuffle())
-    lsorg = lombscargle(taxis, signal)
-    lossold = loss(lsorg,surr, taxis)
-    i = j = 1
-    while i < N_total && j<N_acc
-        if mod(i,500) ==0
+function (sg::SurrogateGenerator{<:LS})()
+    lsplan, xpower, n, dist = sg.init
+    t = sg.method.t
+    tol = sg.method.tol
+    #@show lsplan, xpower, n
+    s = surrogate(sg.x, RandomShuffle())
+    #@show sg.x== s
+    lsplan = LombScargle.plan(t,s, fit_mean=false)
+    s_ls = lombscargle(lsplan)
+    #@show sum(xpower)
+    #@show xpower== s_ls.power
+    #@show xpower
+    #@show s_ls.power
+    lossold = evaluate(dist,xpower, s_ls.power)
+    i = j = 0
+    newsurr = zero(s)
+    while i < sg.method.N_total && j<sg.method.N_acc
+        if mod(i,2000) ==0
             @show i, j,lossold
+            #@show sum(xpower), xpower[1:5]
+            #@show sum(s_ls.power), s_ls.power[1:5]
         end
+
         k,l = sample(1:n,2, replace=false)
         #@show k,l
-        newsurr = copy(surr)
-        newsurr[[k,l]] = surr[[l,k]]
-        lossnew = loss(lsorg, newsurr, taxis)
+        copy!(newsurr, s)
+        #@show length(newsurr)
+        #@show newsurr[1]
+        newsurr[[k,l]] .= s[[l,k]]
+        lsplan = LombScargle.plan(t, newsurr, fit_mean=false)
+        s_ls = lombscargle(lsplan)
+        lossnew = evaluate(dist,xpower, s_ls.power)
+        #@show lossnew, lossold
         if lossnew < lossold
             lossnew <= tol && break
             #@show "update"
-            surr, lossold = newsurr, lossnew
+            s, lossold = copy(newsurr), lossnew
+            #@show length(s)
             j += 1
         #=else
             ## Implement drawing with a probability p
@@ -56,5 +84,6 @@ function surrogate(signal,taxis,sg::LS; N_total=10000, N_acc=2000, tol=1)
         end
         i+=1
     end
-    return surr
+    @info i,j, lossold
+    return s
 end
