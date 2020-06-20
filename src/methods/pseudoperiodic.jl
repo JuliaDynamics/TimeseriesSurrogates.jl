881ead965b826acc4e230b55cb27700e60ff0d34
Datseris <datseris.george@gmail.com>
2020-06-20 21:50:28 +0200

    add description of cycle_shuffle

--- src/methods/pseudoperiodic.jl
+++ src/methods/pseudoperiodic.jl
@@ -7,7 +7,7 @@ Create surrogates suitable for pseudo-periodic signals. They retain the periodic
 of the signal, while inter-cycle dynamics that are either deterministic or correlated
 noise are destroyed (for appropriate `ρ` choice).
 Therefore these surrogates are suitable to test the null hypothesis
-that the signal is periodic with uncorrelated noise[^Small2001].
+that the signal is a periodic orbit with uncorrelated noise[^Small2001].
 
 Arguments `d, τ, ρ` are as in the paper, the embedding dimension, delay time and
 noise radius. The method works by performing a delay coordinates ambedding via the
@@ -18,6 +18,9 @@ the method proposed in the paper in the function [`noiseradius`](@ref).
 The argument `shift` is not discussed in the paper. If `shift=false`
 we adjust the algorithm so that there is little phase shift between the
 periodic component of the original and surrogate data.
+
+See also [`CycleShuffle`](@ref).
+
 
 [^Small2001]: Small et al., Surrogate test for pseudoperiodic time series data, [Physical Review Letters, 87(18)](https://doi.org/10.1103/PhysRevLett.87.188101)
 """
