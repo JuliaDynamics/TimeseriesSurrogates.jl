043d6147fe806eb5156aa48d3a7a82e20a61b2c0
Datseris <datseris.george@gmail.com>
2020-10-31 15:14:09 +0100

    runnable code

--- src/methods/ar.jl
+++ src/methods/ar.jl
@@ -1,6 +1,5 @@
-using DSP, Random, ARFIMA
-
-lpcmethod = LPCLevinson()
+using DSP, Random
+export AutoRegressive
 
 """
     AutoRegressive(n, method = LPCLevinson())
@@ -18,16 +17,16 @@ struct AutoRegressive{M} <: Surrogate
     n::Int
     m::M
 end
-AutoRegressive(n::Int, m::M = LPCLevinson()) where {M} = AutoRegressive{M}(n, m)
+AutoRegressive(n::Int) = AutoRegressive(n, LPCLevinson())
 
 function surrogenerator(x, method::AutoRegressive)
     φ, e = lpc(x, method.n, method.m)
-    init = (d = Normal(0, σ), φ = φ, z = zeros(eltype(x), length(x)+length(φ)), r = copy(x))
+    init = (d = Normal(0, std(x)), φ = φ)
     return SurrogateGenerator(method, x, init)
 end
 
 function (sg::SurrogateGenerator{<:AutoRegressive})()
-    n, φ, σ = length(sg.x), sg.init.φ, sg.init.d
+    n, φ, d = length(sg.x), sg.init.φ, sg.init.d
     return autoregressive(d, n, φ)
 end
 
@@ -35,7 +34,7 @@ function autoregressive(d::Normal{T}, n, φ) where {T}
     f = length(φ)
     z = zeros(T, n+length(φ))
     @inbounds for i in 1:length(φ); z[i] = rand(d); end
-    @inbounds for j in f+1:length(z)
+    @inbounds for i in f+1:length(z)
         s = zero(T)
         for j in 1:f
             s += φ[j]*z[i-j]
