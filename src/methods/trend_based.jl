02cd6ff3713d0b69551435dac69c3822dab30adc
Kristian Haaga <kahaaga@gmail.com>
2022-01-19 17:26:33 +0100

    Fix missing docs

--- src/methods/trend_based.jl
+++ src/methods/trend_based.jl
@@ -5,19 +5,23 @@ export TFTDRandomFourier
     TFTDRandomFourier()
 
 The `TFTDRandomFourier` surrogate was proposed by Lucio et al. (2012)[^Lucio2012] as 
-a modification to the random Fourier [^Theiler1991] and truncated random 
-Fourier[^Nakamura2006] surrogates that allows for generating surrogates from strongly 
-nonstationary time series. 
+a combination of truncated Fourier surrogates[^Nakamura2006] ([`TFTS`](@ref)) and 
+detrend-retrend surrogates.
 
 The `TFTD` part of the name comes from the fact that it uses a combination of truncated 
 Fourier transforms (TFT) and de-trending and re-trending (D) the time series 
-before and after surrogate generation. 
+before and after surrogate generation. Hence, it can be used to generate surrogates also from 
+(strongly) nonstationary time series. 
 
-[^Theiler1991]: J. Theiler, S. Eubank, A. Longtin, B. Galdrikian, J. Farmer, Testing for nonlinearity in time series: The method of surrogate data, Physica D 58 (1–4) (1992) 77–94.
+## Implementation details
+
+Here, a best-fit linear trend is removed/added from the signal prior to and after generating 
+the random Fourier signal. In principle, any trend can be removed, but so far, we only provide
+the linear option.
+
 [^Nakamura2006]: Nakamura, Tomomichi, Michael Small, and Yoshito Hirata. "Testing for nonlinearity in irregular fluctuations with long-term trends." Physical Review E 74.2 (2006): 026205.
 [^Lucio2012]: Lucio, J. H., Valdés, R., & Rodríguez, L. R. (2012). Improvements to surrogate data methods for nonstationary time series. Physical Review E, 85(5), 056202.
 """
-
 struct TFTDRandomFourier <: Surrogate
     phases::Bool
     fϵ
