9eba9957b72571cdbad87e6f453e9e3bfb5861a8
Kristian Haaga <kahaaga@gmail.com>
2022-01-20 14:03:32 +0100

    PseudoPeriodicTwin

---
+++ api2/pseudoperiodictwin.jl
@@ -0,0 +1,23 @@
+using BenchmarkTools, Random
+
+####################################
+# PseudoPeriodicTwin()
+####################################
+rng = MersenneTwister(1234)
+rng2 = MersenneTwister(1234)
+n = 1000
+x = cos.(range(0, 20π, length = n)) .+ randn(n)*0.05
+d, τ = 2, 6
+ρ = noiseradius(x, d, τ, 0.02:0.02:0.5)
+ppstwin = surrogenerator(x, PseudoPeriodicTwin(d, τ, ρ), rng)
+ppstwin2 = surrogenerator(x, PseudoPeriodicTwin2(d, τ, ρ), rng2)
+ppstwin(); ppstwin2();
+
+ppstwin_allo  = @ballocated sg() setup = (sg = $ppstwin)
+ppstwin2_allo = @ballocated sg() setup = (sg = $ppstwin2)
+ppstwin_time  = @belapsed sg() setup = (sg = $ppstwin)
+ppstwin2_time = @belapsed sg() setup = (sg = $ppstwin2)
+
+ppstwin_allo = (ppstwin_allo -  ppstwin2_allo)/ppstwin_allo * 100
+ppstwin_time = (ppstwin_time -  ppstwin2_time)/ppstwin_time * 100
+@show ppstwin_allo, ppstwin_time
--- src/methods/pseudoperiodic_twin.jl
+++ src/methods/pseudoperiodic_twin.jl
@@ -169,3 +169,131 @@ function (sg::SurrogateGenerator{<:PseudoPeriodicTwin})()
     
     return Dataset(s)
 end
+
+export PseudoPeriodicTwin2
+
+struct PseudoPeriodicTwin2{T<:Real, P<:Real, D<:PreMetric} <: Surrogate
+    d::Union{Nothing, Int}
+    τ::Union{Nothing, Int}
+    δ::T
+    ρ::P
+    metric::D
+    
+    function PseudoPeriodicTwin2(d::Int, τ::Int, δ::T = 0.2, ρ::P = 0.1, metric::D = Euclidean()) where {T, P, D}
+        new{T, P, D}(d, τ, δ, ρ, metric)
+    end
+    
+    function PseudoPeriodicTwin2(δ::T = 0.2, ρ::P = 0.1, metric::D = Euclidean()) where {T, P, D}
+        new{T, P, D}(nothing, nothing, δ, ρ, metric)
+    end
+end
+
+""" 
+    _prepare_embed(x::AbstractVector, d, τ) → Dataset
+    _prepare_embed(x::Dataset, d, τ) → Dataset
+
+Prepate input data for surrogate generation. If input is a vector, embed it using 
+the provided parameters. If input as a dataset, we assume it already represents an 
+orbit.
+"""
+function _prepare_embed end
+_prepare_embed(x::AbstractVector, d, τ) = embed(x, d, τ)
+_prepare_embed(x::Dataset, d, τ) = x
+
+
+function surrogenerator(x::Union{AbstractVector, Dataset}, pp::PseudoPeriodicTwin2, rng = Random.default_rng())
+    d, τ, δ, metric = getfield.(Ref(pp), (:d, :τ, :δ, :metric))
+    ρ = getfield.(Ref(pp), (:ρ))
+    
+    pts = _prepare_embed(x, d, τ)
+    
+    Nx = length(x)
+    Npts = length(pts)
+ 
+    dists = pairwise(metric, Matrix(pts), dims = 1)
+    normalisedδ = δ*maximum(dists)
+    
+    T = eltype(pts)
+    R = zeros(T, Npts, Npts)
+    
+    # Recurrence matrix
+    for j = 1:Npts
+        for i = 1:Npts
+            R[i, j] = normalisedδ - dists[i, j] >= 0 ? 1.0 : 0.0
+        end
+    end
+    
+    # Identify twins 
+    #println("R contains $(count(R .== 1)/length(R)*100)% black dots")
+    twins_i = Vector{Int}(undef, 0)
+    twins_j = Vector{Int}(undef, 0)
+    for j = 1:Npts
+        for i = 1:Npts
+            if i !== j && all(R[:, i] .≈ R[:, j])
+                push!(twins_i, i)
+                push!(twins_j, j)
+            end
+        end
+    end
+    #println("Found $(length(twins)) twins")
+    
+    twins = Dict{Int,Vector{Int}}()
+    
+    # For every point that has a twin, store the indices of all of its twins
+    for twi in unique(twins_i) 
+        twins[twi] = twins_j[findall(twins_i .== twi)]
+    end
+    for twj in unique(twins_j)
+        twins[twj] = twins_i[findall(twins_j .== twj)]
+    end
+    
+    # Sampling weights (exclude the point itself)
+    W = [pweights(exp.(-dists[setdiff(1:Npts, i), i] / ρ)) for i = 1:Npts]
+
+    # The surrogate will be a vector of vectors (if pts is a Dataset, then
+    # the eltype is SVector).
+    PT = eltype(pts.data)
+    s = Vector{PT}(undef, Nx)
+
+    init = (pts = pts, Nx = Nx, Npts = Npts, dists = dists, R = R, twins = twins, W = W)
+    return SurrogateGenerator2(pp, x, s, init, rng)
+end
+
+
+function (sg::SurrogateGenerator2{<:PseudoPeriodicTwin2})()
+    pts, Nx, Npts, twins, W = getfield.(Ref(sg.init), (:pts, :Nx, :Npts, :twins, :W))
+    ρ = getfield.(Ref(sg.method), (:ρ))
+    s = sg.s
+
+    # Randomly pick a point from the state space as the starting point for the surrogate.
+    n = 1
+    i = rand(sg.rng, 1:Npts)
+    s[n] = pts[i]
+    
+    while n < Nx
+        # Look for possible twins of the point xᵢ. If any twin exists, jump to one of them 
+        # with probability 1/nⱼ, where nⱼ are the number of twins for the point xᵢ.
+        if haskey(twins, i)
+            # sample uniformly (with probability 1/ntargettwins) over possible target twins
+            j = twins[i][rand(sg.rng, 1:length(twins[i]))]
+            s[n] = pts[j]
+            i = j
+            n += 1
+        end
+        
+        # The orbit moves on from the current point xᵢ to a randomly selected point 
+        # on the attractor. Closer points are more likely to be selected, and points 
+        # further away are less likely to be selected. The sampling probability 
+        # of the next point xⱼ decreases exponentially with increasing distance
+        # from the current point xᵢ. Probabilities for jumping from xᵢ to any other 
+        # point have been pre-computed, and are stored in W[i].
+        j = sample(sg.rng, 1:Npts, W[i])
+        s[n] = pts[j]
+        i = j
+        n += 1
+    end
+    
+    s[Nx] = pts[sample(1:Npts, W[i])[1]]
+    
+    return Dataset(s)
+end
