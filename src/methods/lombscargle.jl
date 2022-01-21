b96bdaa8fc3208795f9af996692ec83308519b31
Kristian Haaga <kahaaga@gmail.com>
2022-01-21 22:04:40 +0100

    Clean-up LombScargle

--- src/methods/lombscargle.jl
+++ src/methods/lombscargle.jl
@@ -30,103 +30,10 @@ struct IrregularLombScargle{T<:AbstractVector, S<:Real} <: Surrogate
     n_acc::Int
     q::Int
 end
-
-IrregularLombScargle(t; tol = 1.0, n_total = 10000, n_acc = 1000, q = 1) = IrregularLombScargle(t, tol, n_total, n_acc, q)
+IrregularLombScargle(t; tol = 1.0, n_total = 10000, n_acc = 1000, q = 1) = IrregularLombScargle2(t, tol, n_total, n_acc, q)
 
 
 function surrogenerator(x, method::IrregularLombScargle, rng = Random.default_rng())
-    # Plan Lombscargle periodogram. Use default flags.
-    lsplan = LombScargle.plan(method.t, x, fit_mean = false)
-
-    # Compute initial periodogram.
-    x_ls = lombscargle(lsplan)
-
-    # We have to copy the power vector here, because we are reusing `lsplan` later on
-    xpower = copy(x_ls.power)
-
-    # Use Minkowski distance of order q
-    dist = Distances.Minkowski(method.q)
-
-    init = (lsplan = lsplan, xpower = xpower, n = length(x), dist = dist)
-    return SurrogateGenerator(method, x, init, rng)
-end
-
-
-function (sg::SurrogateGenerator{<:IrregularLombScargle})()
-    lsplan, xpower, n, dist = sg.init
-    t = sg.method.t
-    tol = sg.method.tol
-    rng = sg.rng
-
-    # When re-computing the Lomb-Scargle periodogram, we will use the 
-    # `_periodogram!` method, which re-uses the lsplan with a shuffled 
-    # time vector. This is the same as shuffling the signal, so the 
-    # surrogate starts out as a shuffled version of `t`.
-    s = surrogate(t, RandomShuffle(), rng)
-    
-    # Power spectrum for the randomly shuffled signal.
-    spower = LombScargle._periodogram!(lsplan.P, s, lsplan)
-
-    # Compare power spectra for original (`xpower`) and randomly shuffled signal (`spower`).
-    lossold = Distances.evaluate(dist, xpower, spower)
-
-    # Initialize a new candidate surrogate.
-    candidate_s = zero(s)
-
-    i = j = 0
-    while i < sg.method.n_total && j < sg.method.n_acc
-        if mod(i, 2000) == 0
-            @info "iterations: $i, swaps: $j, loss: $lossold"
-        end
-
-        # Initially, the new surrogate is identical to the existing surrogate.
-        copy!(candidate_s, s)
-
-        # Swap two random points and re-compute power spectrum for the candidate.
-        k, l = sample(rng, 1:n, 2, replace = false)
-        candidate_s[[k, l]] = s[[l, k]]
-        spower = LombScargle._periodogram!(lsplan.P, candidate_s, lsplan)
-
-        # If spectra are more similar after the swap, accept the new 
-        # surrogate. Otherwise, do a new swap.
-        lossnew = evaluate(dist, xpower, spower)
-
-        if lossnew < lossold
-            lossnew <= tol && break
-            s = copy(candidate_s)
-            lossold = lossnew
-            j += 1
-        end
-        i += 1
-    end
-    @info "Terminated simulated annealing process after $i iterations and $j swaps. Loss: $lossold"
-
-    # Use the permutation of the time vector to permute the signal vector
-    # This gives us the inverse permutation from t to perm
-    perm = sortperm(sortperm(s))
-
-    # Check if this worked as expected
-    @assert t[perm] == s
-    
-    # Re-scale back to original time series values.
-    return sg.x[perm]
-end
-
-
-
-struct IrregularLombScargle2{T<:AbstractVector, S<:Real} <: Surrogate
-    t::T
-    tol::S
-    n_total::Int
-    n_acc::Int
-    q::Int
-end
-
-export IrregularLombScargle2
-IrregularLombScargle2(t; tol = 1.0, n_total = 10000, n_acc = 1000, q = 1) = IrregularLombScargle2(t, tol, n_total, n_acc, q)
-
-
-function surrogenerator(x, method::IrregularLombScargle2, rng = Random.default_rng())
     # Plan Lombscargle periodogram. Use default flags.
     lsplan = LombScargle.plan(method.t, x, fit_mean = false)
 
@@ -156,11 +63,11 @@ function surrogenerator(x, method::IrregularLombScargle2, rng = Random.default_r
         candidate = candidate,
     )
 
-    return SurrogateGenerator2(method, x, s, init, rng)
+    return SurrogateGenerator(method, x, s, init, rng)
 end
 
 
-function (sg::SurrogateGenerator2{<:IrregularLombScargle2})()
+function (sg::SurrogateGenerator{<:IrregularLombScargle})()
     lsplan, xpower, n, dist, candidate = sg.init
     t, tol = sg.method.t, sg.method.tol
     x, s, rng = sg.x, sg.s, sg.rng
