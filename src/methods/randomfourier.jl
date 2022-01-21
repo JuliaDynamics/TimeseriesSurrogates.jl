92493f55ec25d8c7c6fb98718409a61860d91dd3
Kristian Haaga <kahaaga@gmail.com>
2022-01-21 22:10:08 +0100

    Clean-up RandomFourier

--- src/methods/randomfourier.jl
+++ src/methods/randomfourier.jl
@@ -1,4 +1,5 @@
 export RandomFourier, FT
+
 """
     RandomFourier(phases = true) <: Surrogate
 
@@ -29,37 +30,6 @@ function surrogenerator(x::AbstractVector, rf::RandomFourier, rng = Random.defau
     inverse = plan_irfft(forward*x, length(x))
     m = mean(x)
     𝓕 = forward*(x .- m)
-    init = (inverse = inverse, m = m, 𝓕 = 𝓕)
-    return SurrogateGenerator(rf, x, init, rng)
-end
-
-function (rf::SurrogateGenerator{<:RandomFourier})()
-    inverse, m, 𝓕 = getfield.(Ref(rf.init), (:inverse, :m, :𝓕))
-    n = length(𝓕)
-    r = abs.(𝓕)
-    ϕ = abs.(𝓕)
-    if rf.method.phases
-        randomised_ϕ = rand(rf.rng, Uniform(0, 2π), n)
-        new_𝓕 = r .* exp.(randomised_ϕ .* 1im)
-    else
-        randomised_r = r .* rand(rf.rng, Uniform(0, 2π), n)
-        new_𝓕 = randomised_r .* exp.(ϕ .* 1im)
-    end
-    return inverse*new_𝓕 .+ m
-end
-
-export RandomFourier2
-struct RandomFourier2 <: Surrogate
-    phases::Bool
-end
-RandomFourier2() = RandomFourier2(true)
-const FT2 = RandomFourier2
-
-function surrogenerator(x::AbstractVector, rf::RandomFourier2, rng = Random.default_rng())
-    forward = plan_rfft(x)
-    inverse = plan_irfft(forward*x, length(x))
-    m = mean(x)
-    𝓕 = forward*(x .- m)
     shuffled𝓕 = zero(𝓕)
     s = similar(x)
     n = length(𝓕)
@@ -72,7 +42,7 @@ function surrogenerator(x::AbstractVector, rf::RandomFourier2, rng = Random.defa
     return SurrogateGenerator2(rf, x, s, init, rng)
 end
 
-function (sg::SurrogateGenerator2{<:RandomFourier2})()
+function (sg::SurrogateGenerator{<:RandomFourier})()
     inverse, m, coeffs, n, r, ϕ, shuffled𝓕 = 
         getfield.(Ref(sg.init), 
         (:inverse, :m, :coeffs, :n, :r, :ϕ, :shuffled𝓕))
