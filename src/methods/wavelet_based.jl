403248560cc553c4470498c219f6944816ab3aee
Kristian Agasøster Haaga <kahaaga@gmail.com>
2022-01-27 15:17:43 +0100

    Describe what `RandomCascade` surrogates preserve

--- src/methods/wavelet_based.jl
+++ src/methods/wavelet_based.jl
@@ -265,15 +265,17 @@ end
 """
     RandomCascade(paddingmode::String = "zeros")
 
-A random cascade wavelet surrogate (Paluš, 2008)[^Paluš2008].
+A random cascade multifractal wavelet surrogate (Paluš, 2008)[^Paluš2008].
 
 If the input signal length is not a power of 2, the signal must be 
 padded before the surrogate is constructed. `paddingmode` determines 
 how the signal is padded. Currently supported padding modes: `"zeros"`.
-
 The final surrogate (constructed from the padded signal) is subset
 to match the length of the original signal.
 
+Random cascade surrogate preserve multifractal properties of the input 
+time series, that is, interactions among dyadic scales and nonlinear 
+dependencies[^Paluš2008].
 
 [^Paluš2008]: Paluš, Milan (2008). Bootstrapping Multifractals: Surrogate Data from Random Cascades on Wavelet Dyadic Trees. Physical Review Letters, 101(13), 134101–. doi:10.1103/PhysRevLett.101.134101
 """
