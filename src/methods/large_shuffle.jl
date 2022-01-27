e495e264723d8b8fb2622c8906615401c6006e94
Kristian Agasøster Haaga <kahaaga@gmail.com>
2022-01-27 14:22:51 +0100

    Clarify that circshift is random for BlockShuffle

--- src/methods/large_shuffle.jl
+++ src/methods/large_shuffle.jl
@@ -21,8 +21,8 @@ A block shuffle surrogate constructed by dividing the time series
 into `n` blocks of roughly equal width at random indices (end
 blocks are wrapped around to the start of the time series).
 
-If `shift` is `true`, then the input signal is circularly shifted prior 
-to picking blocks.
+If `shift` is `true`, then the input signal is circularly shifted by a 
+random number of steps prior to picking blocks.
 
 Block shuffle surrogates roughly preserve short-range temporal properties
 in the time series (e.g. correlations at lags less than the block length),
