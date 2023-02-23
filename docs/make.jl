71253e1b643ee882cdb4432bb18be852ba635707
Datseris <datseris.george@gmail.com>
2023-02-23 09:09:29 +0000

    new doc style

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,45 @@
 cd(@__DIR__)
+using TimeseriesSurrogates
+
+pages = [
+    "Documentation" => "index.md",
+    "man/whatisasurrogate.md",
+    "Example applications" => [
+        "Shuffle-based" => "methods/randomshuffle.md",
+        "Fourier-based" => "methods/fourier_surrogates.md",
+        "Wavelet-based" => "methods/wls.md",
+        "Pseudo-periodic" => "methods/pps.md",
+        "Pseudo-periodic twin" => "methods/ppts.md",
+        "Multidimensional surrogates" => "methods/multidim.md",
+        "Surrogates for irregular timeseries" => "collections/irregular_surrogates.md",
+        "Surrogates for nonstationary timeseries" => "collections/nonstationary_surrogates.md"
+    ],
+    "man/exampleprocesses.md",
+    "contributor_guide.md"
+]
+
+import Downloads
+Downloads.download(
+    "https://raw.githubusercontent.com/JuliaDynamics/doctheme/master/build_docs_with_style.jl",
+    joinpath(@__DIR__, "build_docs_with_style.jl")
+)
+include("build_docs_with_style.jl")
+
+build_docs_with_style(pages, TimeseriesSurrogates, StateSpaceSets;
+    expandfirst = ["index.md"],
+    authors = "Kristian Agasøster Haaga, George Datseris",
+)
+
+#=
+
+
+
+
+
+
+
+
+cd(@__DIR__)
 using Pkg
 CI = get(ENV, "CI", nothing) == "true" || get(ENV, "GITHUB_TOKEN", nothing) !== nothing
 Pkg.activate(@__DIR__)
@@ -32,22 +73,6 @@ Themes.compile(joinpath(@__DIR__, "juliadynamics-dark.scss"), joinpath(@__DIR__,
 cd(@__DIR__)
 ENV["JULIA_DEBUG"] = "Documenter"
 
-PAGES = [
-    "Documentation" => "index.md",
-    "man/whatisasurrogate.md",
-    "Example applications" => [
-        "Shuffle-based" => "methods/randomshuffle.md",
-        "Fourier-based" => "methods/fourier_surrogates.md",
-        "Wavelet-based" => "methods/wls.md",
-        "Pseudo-periodic" => "methods/pps.md",
-        "Pseudo-periodic twin" => "methods/ppts.md",
-        "Multidimensional surrogates" => "methods/multidim.md",
-        "Surrogates for irregular timeseries" => "collections/irregular_surrogates.md",
-        "Surrogates for nonstationary timeseries" => "collections/nonstationary_surrogates.md"
-    ],
-    "man/exampleprocesses.md",
-    "contributor_guide.md"
-]
 
 makedocs(
     modules = [TimeseriesSurrogates, DynamicalSystems],
