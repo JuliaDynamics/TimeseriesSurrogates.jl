4eef0db4f73a976c3296b9a475ebb6e2dd90157c
Datseris <datseris.george@gmail.com>
2023-03-21 13:14:37 +0000

    finish code and doc of surrogate test

--- src/core/surrogate_test.jl
+++ src/core/surrogate_test.jl
@@ -1,35 +1,55 @@
-import StatsAPI: pvalue
+# import StatsAPI: pvalue
 import Random: AbstractRNG
 export SurrogateTest, pvalue
 
 abstract type AbstractSurrogateTest end
 
-
 """
-    SurrogateTest(f::Ffunction, method::Surrogate; kwargs...)
+    SurrogateTest(f::Function, x, method::Surrogate; kwargs...)
 
 Initialize a surrogate test to be used in [`pvalue`](@ref).
 The tests requires as input a function `f` that given a timeseries `x` it
 outputs a real number, and a method of how to generate surrogates.
+`f` is the function that computes the discriminatory statistic.
+
+## Keywords
+
+- `rng = Random.default_rng()`: a random number generator.
+- `n::Int = 10_000`: how many surrogates to generate and compute `f` on.
 """
-struct SurrogateTest{F<:Function, S<:Surrogate, R<:AbstractRNG} <: AbstractSurrogateTest
+struct SurrogateTest{F<:Function, S<:SurrogateGenerator, X<:Real} <: AbstractSurrogateTest
     f::F
-    method::S
-    rng::R
+    sgen::S
     n::Int
     # fields that are filled whenever a function is called
     # for pretty printing or for keeping track of results
+    rval::X
     vals::Vector{X}
-    rval::RefValue{X}
+    isfilled::Base.RefValue{Bool}
 end
 
-
-function SurrogateTest(f::F, s::S; kwargs...) where {F<:Function, S<:Surrogate}
-    # code
+function SurrogateTest(f::F, x, s::Surrogate;
+        rng = Random.default_rng(), n = 10_000
+    ) where {F<:Function}
+    sgen = surrogenerator(x, s, rng)
+    rval = f(x)
+    X = typeof(rval)
+    vals = zeros(X, n)
+    return SurrogateTest{F, typeof(sgen), X}(f, sgen, n, rval, vals, Ref(false))
 end
 
-function StatsAPI.pvalue(test::SurrogateTest, x::Input; tail = :right)
-    fill_surrogate_test!(test, x)
+function fill_surrogate_test!(test::SurrogateTest)
+    test.isfilled[] && return
+    # TODO: Threading here.
+    for i in 1:test.n
+        test.vals[i] = test.f(test.sgen())
+    end
+    test.isfilled[] = true
+    return
+end
+
+function pvalue(test::SurrogateTest; tail = :right)
+    fill_surrogate_test!(test)
     # estimate pvalue from res.vals
     p = whatever
     return p
