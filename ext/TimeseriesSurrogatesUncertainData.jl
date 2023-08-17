55defadeb2939b87787dc24f85febd5d0bee80b4
Datseris <datseris.george@gmail.com>
2023-08-17 16:09:21 +0100

    port uncertain data to extensions file

---
+++ ext/TimeseriesSurrogatesUncertainData.jl
@@ -0,0 +1,5 @@
+import UncertainData: UncertainDataset, UncertainIndexDataset, UncertainValueDataset, resample
+
+TimeseriesSurrogates.surrogate(x::UncertainDataset, method::Surrogate) = surrogate(resample(x), method)
+TimeseriesSurrogates.surrogate(x::UncertainValueDataset, method::Surrogate) = surrogate(resample(x), method)
+TimeseriesSurrogates.surrogate(x::UncertainIndexDataset, method::Surrogate) = surrogate(resample(x), method)
